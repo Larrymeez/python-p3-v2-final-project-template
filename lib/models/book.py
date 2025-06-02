e65a898dfac5f15a4417abbc38bfe70b29bb2107
Larrymeez <meezlarry@gmail.com>
2025-06-02 18:54:03 +0300

    adds Book relationship

---
+++ lib/models/book.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy.orm import relationship
+from lib.db.setup import Base
+
+class Book(Base):
+    __tablename__ = 'books'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    author = Column(String)
+    is_available = Column(Boolean, default=True)
+
+    lendings = relationship("Lending", back_populates="book")
+
+    def __repr__(self):
+        return f"<Book(title={self.title}, author={self.author}, available={self.is_available})>"
+
